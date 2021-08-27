4e83b4eba94ca9e3453f51919edccfaac740718d
Catcheryp <2281718195@qq.com>
2021-08-28 07:20:51 +0800

    适配器模式

---
+++ adapter/ducks/src/main/java/DuckTestDrive.java
@@ -0,0 +1,21 @@
+public class DuckTestDrive {
+    public static void main(String[] args) {
+        Duck duck = new MallardDuck();
+
+        Turkey turkey = new WildTurkey();
+        Duck turkeyAdapter = new TurkeyAdapter(turkey);
+
+        System.out.println("The Turkey says...");
+        turkey.gobble();
+        turkey.fly();
+
+        System.out.println("\nThe Duck says...");
+        testDuck(duck);
+    }
+
+    static void testDuck(Duck duck){
+        duck.quack();
+        duck.fly();
+
+    }
+}
